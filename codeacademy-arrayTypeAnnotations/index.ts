7dd9fd2bdd4f0ac23d7ae4cb284db103bc4f4f2b
Alvin Cho <alvinxcho2@gmail.com>
2023-01-18 00:06:00 +0000

    Completed arrayTypeAnnotations

---
+++ codeacademy-arrayTypeAnnotations/index.ts
@@ -0,0 +1,4 @@
+let bestNumbers: number[] = [7, 77, 4];
+let bestLunches: string[] = ['chicken soup', 'non-chicken soup'];
+let bestBreakfasts: string[] = ['scrambled eggs', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+let bestBooleans: boolean[] = [true, false];
