f630090e7d87387b396f7d0e570cd8463ec80383
Alvin Cho <alvinxcho2@gmail.com>
2023-02-22 22:10:53 +0000

    Completed inference

---
+++ codeacademy-arrayInference/index.ts
@@ -0,0 +1,7 @@
+let dogTup: [string, string, string, string] = ['dog', 'brown fur', 'curly tail', 'sad eyes'];
+
+// Your code goes here:
+
+let myArr = dogTup.concat(['stubby legs']);
+
+myArr[50] = 'not a dog';
